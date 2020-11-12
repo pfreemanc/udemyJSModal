36045b1f1b30a72904bc0b1b27ce0a5793469674
pfreemanc <pfreemanc@gmail.com>
2020-11-12 15:58:24 -0500

    finished exercise. Comments added.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// selecting the dom elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+// close the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// open the modal
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+// assigning an event listener to each show modal button
+for(let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+// event listeners for the overlay and the x on the modal
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+
+// event listener to close the modal when escape key is pressed. 
+document.addEventListener('keydown', function(event) {
+  if(event.key === 'Escape') {
+    closeModal();
+  }
+})
